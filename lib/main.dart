dc7675f6550c25a976ecfe0e8b0b8c829d31e82b
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-03-07 23:07:48 +0700

    kedua

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:test/app/wiget/scren.dart';
 
 import 'app/routes/app_pages.dart';
-import 'package:get_storage/get_storage.dart';
 
 void main() async {
   await GetStorage.init();
@@ -16,21 +15,19 @@ class myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        return FutureBuilder(
-            future: Future.delayed(Duration(seconds: 1)),
-            builder: (context, snp) {
-              if (snp.connectionState == ConnectionState.waiting) {
-                return scren();
-              } else {
-                return GetMaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  title: "Application",
-                  initialRoute:
-                       Routes.HOME ,
-                  getPages: AppPages.routes,
-                );
-              }
-            });
-      
+    return FutureBuilder(
+        future: Future.delayed(Duration(seconds: 1)),
+        builder: (context, snp) {
+          if (snp.connectionState == ConnectionState.waiting) {
+            return scren();
+          } else {
+            return GetMaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: "Application",
+              initialRoute: Routes.HOME,
+              getPages: AppPages.routes,
+            );
+          }
+        });
   }
 }
